d7e701c4a94d64dd9b17000122402b3066735302
paengjiwoo <shishalu04@gmail.com>
2024-08-25 22:29:44 +0900

    :sparkles: 채팅 페이지 firestore 연결

--- src/routes/chat/$chatRoomId.tsx
+++ src/routes/chat/$chatRoomId.tsx
@@ -1,32 +1,46 @@
 import { Link, createFileRoute, useMatch } from '@tanstack/react-router'
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { IoChevronForwardSharp, IoPersonAdd, IoStar } from 'react-icons/io5';
 import { useProfileData } from '../../hooks/useProfileData';
 import Location from '../../components/common/Location';
 import { BsThreeDots } from 'react-icons/bs';
 import '../../styles/chat/chat.scss';
 import { FaArrowAltCircleUp } from 'react-icons/fa';
+import { Timestamp, addDoc, collection, getFirestore, limit, orderBy, query } from 'firebase/firestore';
+import { app } from '../../firebaseConfig';
+import { useFirestoreQuery } from '../../hooks/useFirestoreQuery';
 
 const Chat: React.FC = () => {
   const { params: { chatRoomId },} = useMatch({ from: '/chat/$chatRoomId' });
 
+  const db = getFirestore(app);
+  const messagesRef = collection(db, `chatRooms/${chatRoomId}/messages`);
+  const messages = useFirestoreQuery(query(messagesRef, orderBy('created_at'), limit(100)));
+
   const { user } = useProfileData();
 
-  const [messages, setMessages] = useState<any[]>([{ text: '벌써 다음달이 가을이네요 참...', type: 'receive'}]);
-  const [inputValue, seInputValue] = useState('');
+  const inputValue = useRef<HTMLInputElement>(null);
 
   const sendMessage = () => {
-    setMessages(prev => [...prev, { text: inputValue, type: 'send' }]);
-    seInputValue('');
-    console.log(inputValue);
+    const trimmedMessage = inputValue.current ? inputValue.current.value.trim() : '';
+    if (inputValue.current && trimmedMessage) {
+      const docData = {
+        chat_id: messages.length + 1,
+        text: trimmedMessage,
+        created_at: Timestamp.fromDate(new Date()),
+        message_type: "message",
+        sender_id: user.id
+      }
+      addDoc(collection(db, `chatRooms/${chatRoomId}/messages`), docData);
+      inputValue.current.value = "";
+      console.log('완료!')
+    }
   };
 
   useEffect(() => {
     if (chatRoomId) {
       console.log(`Joined room: ${chatRoomId}`);
     }
-
-    setMessages(prev => [...prev, { text: inputValue, type: 'receive' }]);
   }, []);
 
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
@@ -86,19 +100,25 @@ const Chat: React.FC = () => {
       {/* 실시간 메세지 표시 영역 */}
       <div className="bubbles">
         
-        {messages.map(message => 
-          message.type === 'receive' ? (
-            <>
-            <div className="bubbles__received">{message.text}</div>
-            <div ref={messagesEndRef} />
-            </>
-          ) : (
-            <>
-            <div className="bubbles__send">{message.text}</div>
-            <div ref={messagesEndRef} />
-            </>
-          )
-        )}
+        {messages.map((message: any, idx: number) => {
+          if (message.message_type === "message"){
+            if (user.id !== message.sender_id) {
+              return (
+                <>
+                <div key={idx} className="bubbles__received">{message.text}</div>
+                <div ref={messagesEndRef} />
+                </>
+              )
+            } else {
+              return (
+                <>
+                <div key={idx} className="bubbles__send">{message.text}</div>
+                <div ref={messagesEndRef} />
+                </>
+              )
+            }
+          }    
+        })}
       </div>
 
       {/* 메세지 입력 영역 */}
@@ -107,9 +127,8 @@ const Chat: React.FC = () => {
           <input
             className="chatinput__input" 
             type="text" 
-            value={inputValue}
             placeholder="message,,," 
-            onChange={(e) => seInputValue(e.target.value)}
+            ref={inputValue}
           />
           <button 
             className="chatinput__button" 
